052d93778484b12f7f754d031bb2ce208ce3b8d8
David Maser <davidemaser@gmail.com>
2017-04-24 13:24:18 -0400

    Update app3.0.js

--- js/app3.0.js
+++ js/app3.0.js
@@ -869,7 +869,7 @@ var core = {
         $.ajax({
             url:'https://cdn.shopify.com/s/files/1/0050/3522/t/22/assets/banners.json',
             success:function(data){
-                core.traverseJSON(false,'',data);
+                core.traverseJSON(false,'',JSON.stringify(data));
             }
         })
     },
