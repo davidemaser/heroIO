bb7ec7ff26647e56de559bad3fe3b0fd6a6c5096
David Maser <davidemaser@gmail.com>
2017-04-24 14:55:59 -0400

    Conversion for ES6

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,12 +5,17 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const jsdoc = require('gulp-jsdoc3');
 
-var paths = {
+let paths = {
     scripts: ['js/**/*.js'],
     css: 'css/**/*'
 };
 
 gulp.task('doc',function(cb) {
-    gulp.src(['README.md','js/app3.0.js'], {read: true})
+    gulp.src(['README.md','js/app4.0.js'], {read: true})
     .pipe(jsdoc(cb));
+});
+gulp.task("default", function () {
+    return gulp.src("js/app4.0.js")
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
 });
